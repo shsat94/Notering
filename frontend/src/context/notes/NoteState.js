7c65cba2bb576b15ba264695f8c7f2f68bedded8
shsat94 <shsat94@gmail.com>
2024-08-30 16:54:19 +0530

    updated

--- frontend/src/context/notes/NoteState.js
+++ frontend/src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import NoteContext from "./NoteContext";
 
 const NoteState = (props) => {
-    const host = "http://localhost:5000";
+    const host = "https://notering-backend.onrender.com";
     const notesInitial = [];
     const [notes, setNotes] = useState(notesInitial);
     
