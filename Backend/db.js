6b9b3292d4f02c2e0d0385becc795d172ddceeb9
shsat94 <shsat94@gmail.com>
2024-08-30 19:51:30 +0530

    updated mongo uri

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI="mongodb+srv://shsat94:Satyarth23$Siri@notering.dig1k.mongodb.net/";
+const mongoURI="mongodb+srv://shsat94:Satyarth23$Siri@notering.dig1k.mongodb.net/?retryWrites=true&w=majority&appName=notering";
 
 const connectToMongo =async ()=>{
     mongoose.connect(mongoURI);
